c5565f625e3b1277a4003263b95354e568622f12
RoseHumphrey <44196911+GordienkoRoman@users.noreply.github.com>
2023-04-18 21:51:32 +0400

    files structure

--- app/src/main/java/com/example/dog_observer/login/LoginActivityViewModel.kt
+++ app/src/main/java/com/example/dog_observer/login/LoginActivityViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.dog_observer.login
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.navigation.NavDirections
 import javax.inject.Inject
 
 class LoginActivityViewModel @Inject constructor(
